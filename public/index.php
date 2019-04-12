1872a76178746a42b2216643247546e02e302bb5
HardCoreWare <bernardocodu@gmail.com>
2019-04-12 13:59:20 -0500

    ok

--- public/index.php
+++ public/index.php
@@ -1,11 +1,8 @@
 <?php
 
 require_once '../app/core/manager.php';
-
 $bigQuery = BigQuery::getInstance('informe-211921');
-
 $sql = "SELECT BUKRS,BUDAT FROM `informe-211921.BALANZA.BSEG_2019_1` LIMIT 100";
-
 $bseg = $bigQuery->select($sql);
 
 ?>
